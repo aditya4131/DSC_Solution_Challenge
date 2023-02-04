c7e13103d0b667bc0332e560c1310f8dbf3276e7
aditya4131 <asriva8@lsu.edu>
2023-02-04 15:49:03 -0600

    First commit.Added project files and firebase dependencies.

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:dsc_project/shared/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: Constants.apiKey,
+            appId: Constants.appId,
+            messagingSenderId: Constants.messagingSenderId,
+            projectId: Constants.projectId));
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:10997500044:web:37ba9add4d4fe6861513a9";
+  static String apiKey = "AIzaSyAMJ8VA38mUvNqCILTvsT2uYF0d01BwuIU";
+  static String messagingSenderId = "10997500044";
+  static String projectId = "dscsolutionsproj";
+}
