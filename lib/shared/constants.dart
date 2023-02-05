3d32c7a01c443718a72fb7fbdf113200afcf01b1
aditya4131 <asriva8@lsu.edu>
2023-02-05 16:56:47 -0600

    removed api key

--- lib/shared/constants.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Constants {
-  static String appId = "1:10997500044:web:37ba9add4d4fe6861513a9";
-  static String apiKey = "AIzaSyAMJ8VA38mUvNqCILTvsT2uYF0d01BwuIU";
-  static String messagingSenderId = "10997500044";
-  static String projectId = "dscsolutionsproj";
-  final primaryColor = const Color(0xFFBD7BBE9);
-}
